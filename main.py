a8985b5f29c64de5250753e4bf17246d51bd1294
PheeNeeck <Ustas184@gmail.com>
2020-05-19 20:53:48 +0300

    Нормальная кодировка кириллицы в json файле

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import json
+import tkinter as tk
 
+library = {"Мастер и Маргарита": ["М.А.Булгаков", "роман", "Фантастика", "1", "1", "1"]}
 
-library = {"Мастер и Маргарита":["М.А.Булгаков", "роман", "Фантастика", "1", "1", "1"]}
+with open("library.lib", "w", encoding="utf-8")as f:
+    json.dump(library, f, indent=4, ensure_ascii=False)
 
-with open("library.lib", "w", encoding="utf8")as f:
-    json.dump(library, f)
+with open("library.lib", "r", encoding="utf-8")as f:
+    print(json.load(f))
