59cc11d7b1f50380177d0dc12ea09d89315623c1
PheeNeeck <Ustas184@gmail.com>
2020-09-27 17:44:34 +0300

    Сделал выгрузку книг в файлы и загрузку книг из файлов

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import json
 import pymysql
+import os
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
-import driver
+import json
+import csv
 
 
 class Book:
@@ -25,15 +26,101 @@ class Connection:
             print("Не удалось подключиться к базе данных!")
 
 
-class Saver:
-    def __init__(self, driver_):
-        self.driver_ = driver_
+class JsonDriverBuilder:
+    def __init__(self, filename):
+        filename = filename.strip()
+        if not str(filename).endswith(".json"):
+            filename = filename + ".json"
+        self.filename = filename
 
-    def write(self):
-        self.driver_.write(self.__list)
+    def write(self, data):
+        with open(self.filename, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False)
 
     def read(self):
-        return self.driver_.read()
+        with open(self.filename, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            con = Connection.connect()
+            with con:
+                cur = con.cursor()
+                for i in data:
+                    new_book = Book(i[1], i[2], i[3])
+                    cur.execute(f"INSERT INTO `bookshelf`.`books` (`name`, `author`, `janr`)\
+                                    VALUES ('{new_book.name}', '{new_book.author}', '{new_book.janr}');")
+                con.commit()
+
+
+class CSVDriverBuilder:
+    def __init__(self, filename):
+        filename = filename.strip()
+        if not filename.endswith(".csv"):
+            filename = filename + ".scv"
+        self.filename = filename
+
+    def write(self, data):
+        with open(self.filename, "w", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            for i in data:
+                writer.writerow(i)
+
+    def read(self):
+        with open(self.filename, "r", encoding="utf-8") as f:
+            file_reader = csv.reader(f, delimiter=",")
+            con = Connection.connect()
+            with con:
+                cur = con.cursor()
+                for i in file_reader:
+                    if i:
+                        new_book = Book(i[1], i[2], i[3])
+                        cur.execute(f"INSERT INTO `bookshelf`.`books` (`name`, `author`, `janr`)\
+                                        VALUES ('{new_book.name}', '{new_book.author}', '{new_book.janr}');")
+                con.commit()
+
+
+class TxtDriverBuilder:
+    def __init__(self, filename):
+        filename = filename.strip()
+        if not str(filename).endswith(".txt"):
+            filename = filename + ".txt"
+        self.filename = filename
+
+    def write(self, data):
+        with open(self.filename, "w", encoding="utf-8") as f:
+            for i in data:
+                json.dump(i, f, ensure_ascii=False)
+                f.write("\n")
+
+    def read(self):
+        with open(self.filename, "r", encoding="utf-8") as f:
+            data = []
+            for i in f.readlines():
+                if len(i) > 1:
+                    data.append(json.loads(i[:-1]))
+            con = Connection.connect()
+            with con:
+                cur = con.cursor()
+                for i in data:
+                    new_book = Book(i[1], i[2], i[3])
+                    cur.execute(f"INSERT INTO `bookshelf`.`books` (`name`, `author`, `janr`)\
+                                    VALUES ('{new_book.name}', '{new_book.author}', '{new_book.janr}');")
+                con.commit()
+
+
+class SaveLoad:
+    def __init__(self, data, filename):
+        if tkgui.writer_var.get() == 1:
+            self.driver = JsonDriverBuilder(filename)
+        elif tkgui.writer_var.get() == 2:
+            self.driver = CSVDriverBuilder(filename)
+        elif tkgui.writer_var.get() == 3:
+            self.driver = TxtDriverBuilder(filename)
+        self.data = data
+
+    def write(self):
+        self.driver.write(self.data)
+
+    def read(self):
+        return self.driver.read()
 
 
 class Actions:
@@ -212,7 +299,6 @@ class Actions:
         Функция сохраняет введенные изменения по книге в библиотеку
         :return:
         """
-        print(book_id)
         con = Connection.connect()
         with con:
             cur = con.cursor()
@@ -377,8 +463,9 @@ class TkGUI:
         self.name_DB_entry.grid(column=5, row=1, padx=10, sticky=W)
         self.name_DB_entry.insert(0, "Bookshelf")
 
-        self.save_conf_button = Button(self.config_tab, text="Подключиться", command=Connection.connect, state="normal")
-        self.save_conf_button.grid(column=1, row=2)
+        self.save_conf_button = Button(self.config_tab, text="Проверить подключение", command=Connection.connect,
+                                       state="normal")
+        self.save_conf_button.grid(column=0, row=2)
 
         # ---------------- Заполение данных "Насторойки выгрузки"--------------------
 
@@ -387,15 +474,29 @@ class TkGUI:
 
         self.writer_var = IntVar()
         self.writer_var.set(1)
-        self.rad1 = ttk.Radiobutton(self.config_tab, text='в JSON', variable=self.writer_var, value=1)
-        self.rad2 = ttk.Radiobutton(self.config_tab, text='в CSV', variable=self.writer_var, value=2)
-        self.rad3 = ttk.Radiobutton(self.config_tab, text='в TXT', variable=self.writer_var, value=3)
-        self.rad1.grid(column=1, row=3)
-        self.rad2.grid(column=2, row=3)
-        self.rad3.grid(column=3, row=3)
+        self.save1 = ttk.Radiobutton(self.config_tab, text='в JSON', variable=self.writer_var, value=1)
+        self.save2 = ttk.Radiobutton(self.config_tab, text='в CSV', variable=self.writer_var, value=2)
+        self.save3 = ttk.Radiobutton(self.config_tab, text='в TXT', variable=self.writer_var, value=3)
+        self.save1.grid(column=1, row=3)
+        self.save2.grid(column=2, row=3)
+        self.save3.grid(column=3, row=3)
+
+        self.save_file_button = Button(self.config_tab, text="Выгрузить в файл", command=self.save_file, state="normal")
+        self.save_file_button.grid(column=0, row=4)
+
+        self.file_string = Label(self.config_tab, text="Имя файла:")
+        self.file_string.grid(column=1, row=4, padx=10, pady=10, sticky=W)
+
+        self.file_entry = Entry(self.config_tab, width="10", state="normal")
+        self.file_entry.grid(column=2, row=4, padx=10, sticky=W)
+
+        self.load_file_button = Button(self.config_tab, text="загрузить", command=self.load_file, state="normal")
+        self.load_file_button.grid(column=3, row=4)
 
         self.result = None
         self.book_id = None
+        self.save_driver = None
+        self.data = None
 
     def main(self):
         self.root.mainloop()
@@ -403,9 +504,6 @@ class TkGUI:
     def search(self):
         self.result = Actions.find_book()
         self.book_id = self.result[0][0]
-
-    def save_config(self):
-        pass
 
     def next_result(self):
         self.book_id = Actions.next_result(self.result)
@@ -421,6 +519,36 @@ class TkGUI:
 
     def save_book(self):
         Actions.save_book(self.book_id)
+
+    def save_file(self):
+        TkGUI.verify_path(self.file_entry.get())
+        con = Connection.connect()
+        with con:
+            cur = con.cursor()
+            if tkgui.find_var.get() == 1:
+                cur.execute(f"SELECT * FROM `bookshelf`.`books`;")
+            self.data = list(cur.fetchall())
+        self.save_driver = SaveLoad(self.data, self.file_entry.get())
+        self.save_driver.write()
+
+    def load_file(self):
+        self.save_driver = SaveLoad(self.data, self.file_entry.get())
+        self.save_driver.read()
+
+    @staticmethod
+    def verify_path(path):
+        counter = len(path)
+        while counter > 0:
+            print(path[-1], type(path[-1]))
+            if path[-1] == "/" or path[-1] == "\\":
+                path = path[:-1]
+                break
+            else:
+                path = path[:-1]
+                counter -= 1
+        if len(path) and not os.path.exists(path):
+            os.makedirs(path)
+        print(path)
 
 
 if __name__ == '__main__':
